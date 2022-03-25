b20ac4149f94fd7b5f41e4ea1ecba2296b40cb19
SarmenSinanian <sarmen.sinanian@yahoo.com>
2022-03-25 07:00:45 -0700

    Renamed some Day1 files; Added Day2 files (Mock Exam 2 not completed yet)

---
+++ HackerRank2022/Day2/Flipping_The_Matrix(Mock_Test)_Code_Only.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'flippingMatrix' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+#
+
+def flippingMatrix(matrix):
+    # Write your code here
+    # if matrix[0[0]]
+    temp = []
+    if (matrix[0][0]+matrix[0][1])<(matrix[0][2]+matrix[0][3]):
+        temp = [matrix[0][3], matrix[0][2], matrix[0][1], matrix[0][0]]
+        matrix[0] = temp
+    return matrix
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        matrix = []
+
+        for _ in range(2 * n):
+            matrix.append(list(map(int, input().rstrip().split())))
+
+        result = flippingMatrix(matrix)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
