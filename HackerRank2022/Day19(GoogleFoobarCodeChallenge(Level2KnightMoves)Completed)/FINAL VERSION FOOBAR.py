65abd9139499abb8cdbfd4c94b892fd2bf8b77ed
SarmenSinanian <sarmen.sinanian@yahoo.com>
2022-04-12 08:26:02 -0700

    Reuploading yesterdays upload that did not go through to github for some reason. Completed First Half for Level 2 of Google Foobar Code Challenge AKA Knight Moves

---
+++ HackerRank2022/Day19(GoogleFoobarCodeChallenge(Level2KnightMoves)Completed)/FINAL VERSION FOOBAR.py
@@ -0,0 +1,53 @@
+from itertools import product
+from time import process_time
+
+t1_start = process_time()
+
+# for i in range(25):
+
+
+def knight_moves(position1, position2):
+    x, y = position1, position2
+    moves = list(product([x-1, x+1],[y-2, y+2])) + list(product([x-2,x+2],[y-1,y+1]))
+    moves = [(x,y) for x,y in moves if 0 <= x < 8 and 0 <= y < 8]
+    return moves
+
+
+def solution(src, dest):
+    board_dict = {}
+    spots = range(64)
+    values = [(x, y) for x in range(8) for y in range(8)]
+    for i in spots:
+        board_dict[i] = values[i]
+    src_cartesian = board_dict[src]
+    dest_cartesian = board_dict[dest]
+    if src_cartesian == dest_cartesian:
+        return 0
+    moves = knight_moves(src_cartesian[0], src_cartesian[1])
+    if dest_cartesian in moves:
+        return 1
+    move_count = 0
+    list_of_checked_new_moves = []
+    while dest_cartesian not in moves:
+        move_count += 1
+        print(move_count)
+        moves.extend(list_of_checked_new_moves)
+        list_of_checked_new_moves = []
+        for j in moves:
+            new_moves = knight_moves(j[0], j[1])
+            for k in new_moves:
+                if k not in moves:
+                    list_of_checked_new_moves.append(k)
+            if dest_cartesian in list_of_checked_new_moves:
+                break
+    return move_count
+
+
+print(solution(0, 20))
+
+t1_stop = process_time()
+
+print("Elapsed time:", t1_stop, t1_start)
+
+print("Elapsed time during the whole program in seconds:",
+      t1_stop - t1_start)
